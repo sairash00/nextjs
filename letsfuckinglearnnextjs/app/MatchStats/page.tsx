a99dc6c4dde0ee607860354de74ab6e8cb76f3f3
sairash00 <sairash7248@gmail.com>
2026-05-21 07:31:47 +0545

    fixed some errors

--- letsfuckinglearnnextjs/app/MatchStats/page.tsx
+++ letsfuckinglearnnextjs/app/MatchStats/page.tsx
@@ -2,7 +2,7 @@ import SecondaryNavBar from "@/components/SecondaryNavBar";
 import Image from "next/image";
 import React from "react";
 
-const page = () => {
+const page = () =>  {
   return (
     <div className=" w-[80vw] h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-gray-700  bg-gray-800 flex flex-col gap-4  px-8 py-4  ">
       <SecondaryNavBar />
