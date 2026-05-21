c7b118d792f299188989d500432fd6d0b63f5878
sairash00 <sairash7248@gmail.com>
2026-05-21 12:56:26 +0545

    API CALL IMPLEMENTATION

--- letsfuckinglearnnextjs/app/page.tsx
+++ letsfuckinglearnnextjs/app/page.tsx
@@ -4,7 +4,20 @@
 
 import SecondaryNavBar from "@/components/SecondaryNavBar"
 
-const Home = () => {
+const Home = async () => {
+
+    const res = await fetch("https://v3.football.api-sports.io/fixtures&season=2026",{
+        cache: "no-store",
+        headers: {
+          "x-rapidapi-key": process.env.APIKEYFOOTBALL!,
+          "x-rapidapi-host": "v3.football.api-sports.io",
+        },
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+    
+
   return (
     <div className=" w-[80vw] h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-gray-700  bg-gray-800 flex flex-col gap-4  px-8 py-4  " >
      <SecondaryNavBar/>
@@ -12,13 +25,7 @@ const Home = () => {
       <div className="flex  flex-col gap-3 py-3"  >
         <h2 className="text-xl font-bold" >Live Football</h2>
         <div className="flex gap-4 overflow-x-scroll scrollbar-track-transparent w-full h-[22vh] max-h-[24vh]  " >
-          {/* <LiveGameCard/>
-          <LiveGameCard/>
-          <LiveGameCard/>
-          <LiveGameCard/>
-          <LiveGameCard/>
-          <LiveGameCard/>
-          <LiveGameCard/> */}
+      
         </div>
       </div>
 
